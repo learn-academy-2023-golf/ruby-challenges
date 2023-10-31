e845fcdd71e6f293531e27e649b80fe088f26325
TheLavitz <lavitz@gmail.com>
2023-10-31 11:07:18 -0700

    Finished second half of challenges

--- classes-corey-sean.rb
+++ classes-corey-sean.rb
@@ -56,38 +56,95 @@
 
 # As a developer, I can update the status of each instance of class Task when the task has been completed.
 
-class Task
-    attr_accessor :title
+# class Task
+#     attr_accessor :title
 
-    def initialize (title)
-        @title = title
-        @status = 'incomplete'
-    end
+#     def initialize (title)
+#         @title = title
+#         @status = 'incomplete'
+#     end
 
-    def set_title title
-        @title = title
-    end
+#     def set_title title
+#         @title = title
+#     end
 
-    def set_status status
-        @status = status
-    end
+#     def set_status status
+#         @status = status
+#     end
 
-end
+# end
 
-cooking = Task.new('Cooking')
+# cooking = Task.new('Cooking')
 
-cooking.set_title('Pasta')
-cooking.set_status('complete')
+# cooking.set_title('Pasta')
+# cooking.set_status('complete')
 
-p cooking                   # #<Task:0x00000001027803b8 @title="Pasta", @status="complete">
+# p cooking                   # #<Task:0x00000001027803b8 @title="Pasta", @status="complete">
 
 # For the following ColorPalette challenge use initialize and attr_accessor methods in your class.
 
 # As a developer, I can create a class called ColorPalette.
+
+# class ColorPalette
+
+# end
+
 # As a developer, I can create three instances (objects) of class ColorPalette.
 # e.g green = ColorPalette.new
+
+# red = ColorPalette.new
+# green = ColorPalette.new
+# blue = ColorPalette.new
+
 # As a developer, I can initialize each instance of the class ColorPalette with three colors.
 # e.g. green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
+
+# class ColorPalette
+#     def initialize (shade1, shade2, shade3)
+#         @shade1 = shade1
+#         @shade2 = shade2
+#         @shade3 = shade3
+#     end
+# end
+
+# green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
+# p green
+
 # As a developer, I can print the value of each individual color.
+
+# class ColorPalette
+#     attr_accessor :shade1, :shade2, :shade3
+#     def initialize (shade1, shade2, shade3)
+#         @shade1 = shade1
+#         @shade2 = shade2
+#         @shade3 = shade3
+#     end
+# end
+
+# green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
+# p green
+# p green.shade1
+# p green.shade2
+# p green.shade3
+
 # As a developer, I can create a method called all_colors that when called will print a sentence telling me the three colors of a given palette.
+
+class ColorPalette
+    attr_accessor :shade1, :shade2, :shade3
+    def initialize (shade1, shade2, shade3)
+        @shade1 = shade1
+        @shade2 = shade2
+        @shade3 = shade3
+    end
+    
+    def all_colors
+        "The three colors of this pallette are #{@shade1}, #{@shade2}, and #{@shade3}."
+    end
+end
+
+green = ColorPalette.new('Chartreuse', 'Kelly', 'Seafoam')
+p green.all_colors
+
 # As a developer, I can change one or more colors of a given palette.
+green.shade1 = 'Turquoise'
+p green.all_colors
